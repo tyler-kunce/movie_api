64eeaad005410a53537724e31c46d50170020b08
tyler-kunce <tyler.kunce@gmail.com>
2024-03-14 17:39:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,23 +31,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
 
-/* Block of code to limit access to certain origins
-let allowedOrigins = ['http://localhost:8080', 'https://testsite.com'];
+// Block of code to limit access to certain origins
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://testsite.com',
+  'http://localhost:1234',
+  'https://myflix-tk.netlify.app/',
+];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    isBuffer(allowedOrigins.indexOf(origin) === -1); {
-      // If a specific origin isn't found on the list of allowed origins
-        let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-        return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
-*/
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      isBuffer(allowedOrigins.indexOf(origin) === -1);
+      {
+        // If a specific origin isn't found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 let auth = require('./auth')(app);
 
